f23f9fa308038ccd48019a981f00e65d03c16849
scroo-g <js@scroo-g.com>
2024-02-17 00:17:42 +0500

    Change deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		// adapter: adapter({
+		// 	// See below for an explanation of these options
+		// 	routes: {
+		// 		include: ['/*'],
+		// 		exclude: ['<all>']
+		// 	}
+		// })
 		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
+			pages: 'dist',
+			assets: 'dist',
+			precompress: true,
+			fallback: 'index.html'
 		})
 	},
 	preprocess: vitePreprocess()
